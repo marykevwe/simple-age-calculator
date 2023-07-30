4c4928e0dae346fe810390fa0b4a3bc1e54a92ee
unknown <marianaphiare@gmail.com>
2023-07-30 10:02:29 -0400

    create promt for first and second user input

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //create variables to accept first and second user input
+let firstInput = prompt("Enter the first number:");
+let secondInput = prompt("Enter the second number:");
 //create an addition function
+function addition(a, b) {
 //the addition function will return the addition
+return parseInt(a) + parseInt(b);
+}
 //invoke addition function
+console.log(addition(firstInput, secondInput));
