7564029ad0c2d751519a287c8bc497eeb59d99bd
unknown <marianaphiare@gmail.com>
2023-07-31 15:58:34 -0400

    create pseudocods for the calculator

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-//create variables to accept first and second user input
-let firstInput = prompt("Enter the first number:");
-let secondInput = prompt("Enter the second number:");
-//create an addition function
-function addition(a, b) {
-//the addition function will return the addition
-return parseInt(a) + parseInt(b);
-}
-//invoke addition function
-console.log(addition(firstInput, secondInput));
+const prompt = require("prompt-sync")();
+// Prompt the user to enter the birth year
+let birthYear = parseInt(prompt("Enter your birth yeargit :"));
+// Prompt the user to enter the current year
+let currentYear = parseInt(prompt("Enter the current year:"));
+// Calculate the agelet age = currentYear - birthYear;
+
+// Display the calculated age
+console.log("Your age is: " + age);
+
+// Loop through the array
+for (let i = 0; i < numbers.length; i++) {
+      // Check if the number is even  if (numbers[i] % 2 === 0) {
+        // Output the even number    console.log(numbers[i]);
+      }
+    }
+    
