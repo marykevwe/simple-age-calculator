688368ae4fd9c595f6ce5236ebb77ff122609312
unknown <marianaphiare@gmail.com>
2023-07-31 16:24:42 -0400

    create pseudocods for the calculator

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const prompt = require("prompt-sync")();
 // Prompt the user to enter the birth year
 let birthYear = parseInt(prompt("Enter your birth yeargit :"));
 // Prompt the user to enter the current year
@@ -8,10 +7,3 @@ let age = currentYear - birthYear;
 // Display the calculated age
 console.log("Your age is: " + age);
 
-// Loop through the array
-for (let i = 0; i < numbers.length; i++) {
-      // Check if the number is even  if (numbers[i] % 2 === 0) {
-        // Output the even number    console.log(numbers[i]);
-      }
-    }
-    
