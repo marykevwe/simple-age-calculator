fe8c0e8724afd239837c2f6a1d0f9e3c5351bece
unknown <marianaphiare@gmail.com>
2023-07-31 16:09:45 -0400

    create pseudocods for the calculator

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const prompt = require("prompt-sync")();
 let birthYear = parseInt(prompt("Enter your birth yeargit :"));
 // Prompt the user to enter the current year
 let currentYear = parseInt(prompt("Enter the current year:"));
-// Calculate the agelet age = currentYear - birthYear;
-
+// Calculate the age let age = currentYear - birthYear;
+let age = currentYear - birthYear;
 // Display the calculated age
 console.log("Your age is: " + age);
 
