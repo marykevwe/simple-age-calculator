6c4346e39f735389021d3c9e129dcc0a63be3541
unknown <marianaphiare@gmail.com>
2023-08-01 08:39:34 -0400

    create array for even numbers

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log("Your age is: " + age);
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Loop through the array
 for (let i = 0; i < numbers.length; i++) {
-     // Check if the number is even  if 
+     // Check if the number is even  
   if (numbers[i] % 2 === 0) {
      // Output the even number
     console.log(numbers[i]);
