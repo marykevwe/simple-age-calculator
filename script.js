8e8006bc72aa846f5b2bbc05592018710f94f36b
unknown <marianaphiare@gmail.com>
2023-08-01 08:35:05 -0400

    create pseudocods for the loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Prompt the user to enter the birth year
-let birthYear = parseInt(prompt("Enter your birth yeargit :"));
+let birthYear = parseInt(prompt("Enter your birth year :"));
 // Prompt the user to enter the current year
 let currentYear = parseInt(prompt("Enter the current year:"));
 // Calculate the age let age = currentYear - birthYear;
@@ -7,3 +7,9 @@ let age = currentYear - birthYear;
 // Display the calculated age
 console.log("Your age is: " + age);
 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(numbers[i]);
+  }
+}
